f29a78c9ea38751e892f8a155ed1584933421409
Tawfik <tawfik17800@gmail.com>
2022-03-03 11:42:51 +0600

    RGB Generator By Clicing

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    main()
+}
+function main() {
+    var btn = document.getElementById("btn");
+    var body = document.getElementById("root");
+    btn.addEventListener("click", function () {
+        var bgColor = rgbColorGenerator();
+        console.log(bgColor)
+        body.style.backgroundColor = bgColor;
+
+    })
+}
+
+function rgbColorGenerator() {
+    var red = Math.floor(Math.random() * 255)
+    var green = Math.floor(Math.random() * 255)
+    var blue = Math.floor(Math.random() * 255)
+
+    return `rgb(${red},${green},${blue})`;
+}
